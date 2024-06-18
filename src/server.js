40fb0ae0749d6c2840e4e187094eb7edd0af39cb
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-18 23:20:00 +0300

    redact status

--- src/server.js
+++ src/server.js
@@ -24,23 +24,26 @@ export const setupServer = () => {
 
   app.get("/", (req, res) => {
     res.json({
-      message: "Hello World!",
+      status: 200,
+      message: 'Hello World!',
     });
   });
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
-    res.status(200).json({
-      data: contacts,
-      message: 'Successfully found contacts!',
-    });
+     res.json({
+       status: 200,
+       message: 'Successfully found contacts!',
+       data: contacts,
+     });
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
-      return res.status(404).json({
+      return res.json({
+        status: 404,
         message: `Contact with id ${contactId} is invalid.`,
       });
     }
@@ -48,27 +51,31 @@ export const setupServer = () => {
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      return res.status(404).json({
+      return res.json({
+        status: 404,
         message: `Contact with id ${contactId} not found.`,
       });
     }
 
-    res.status(200).json({
-      data: contact,
+    res.json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
   app.use("*", (req, res, next) => {
-    res.status(404).json({
-      message: "Not found",
+    res.json({
+      status: 404,
+      message: 'Not found',
     });
     next();
   });
 
   app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: "Something went wrong",
+    res.json({
+      status: 500,
+      message: 'Something went wrong',
       error: err.message,
     });
     next();
