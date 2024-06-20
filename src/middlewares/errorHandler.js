fbd74bb96dd77359ab8d56061f1e6b2238f2ec0e
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-20 17:46:36 +0300

    add MonooseError to errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 import { HttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
 
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
@@ -6,6 +7,17 @@ export const errorHandler = (err, req, res, next) => {
       status: err.status,
       message: err.name,
       data: err,
+    });
+    return;
+  }
+
+  if (err instanceof MongooseError) {
+    res.status(404).json({
+      status: 404,
+      message: 'NotFoundError',
+      data: {
+        message: 'Contact not found',
+      },
     });
     return;
   }
