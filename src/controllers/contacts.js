55ac18bc239d02ad10e49da526693f0155f44e1b
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-20 17:46:36 +0300

    add MonooseError to errorHandler

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,7 +1,7 @@
 
 import { getAllContacts, getContactById } from '../services/contacts.js';
  import createHttpError from 'http-errors';
-  import mongoose from 'mongoose';
+
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -17,13 +17,18 @@ export const getContactByIdController = async (req, res, next) => {
 
 
 
- if (!mongoose.Types.ObjectId.isValid(contactId)) {
+//  if (!mongoose.Types.ObjectId.isValid(contactId)) {
 
-   next(createHttpError(404, 'Contact not found'));
-   return;
- }
- 
+//    next(createHttpError(404, 'Contact not found'));
+//    return;
+//  }
+
  const contact = await getContactById(contactId);
+   
+    if (!contact) {
+     next(createHttpError(404, `Contact not found`));
+     return;
+   }
 
     res.json({
     status: 200,
