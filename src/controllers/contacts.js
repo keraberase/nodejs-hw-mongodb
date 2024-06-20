368ee5881cf3400d9d3bc1d2410b248c208a7630
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-20 18:09:34 +0300

    add Post-router

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,5 @@
 
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {createContact, getAllContacts, getContactById } from '../services/contacts.js';
  import createHttpError from 'http-errors';
 
 
@@ -33,6 +33,16 @@ export const getContactByIdController = async (req, res, next) => {
     res.json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
     data: contact,
   });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,10 +4,12 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   getContactsController,
   getContactByIdController,
+  createContactController,
 } from '../controllers/contacts.js';
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
 
 export default router;
