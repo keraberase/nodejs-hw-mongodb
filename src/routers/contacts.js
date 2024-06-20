a0f0a97149f27cffe08d400d44cdc9e3b3dab58b
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-21 01:31:53 +0300

    add DELETE-router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,6 +6,7 @@ import {
   getContactByIdController,
   createContactController,
   patchContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
 const router = Router();
 
@@ -13,5 +14,6 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
