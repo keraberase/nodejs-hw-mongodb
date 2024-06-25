7c1cc5ee66e8425545039e9cc744490e36f12785
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-25 11:27:51 +0100

    add validateBody to POST-router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
 
 import {
   getContactsController,
@@ -12,7 +14,7 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
