5cad9176462ce5f10aa23b591eef2730525fd82d
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-25 18:29:33 +0100

    add filter to GET-all-contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -8,25 +8,35 @@ import {
 } from '../services/contacts.js';
  import { parsePaginationParams } from '../utils/parsePaginationParams.js';
   import { parseSortParams } from '../utils/parseSortParams.js';
+   import { parseFilterParams } from '../utils/parseFilterParams.js';
  import createHttpError from 'http-errors';
 
 
-export const getContactsController = async (req, res) => {
-    const { page, perPage } = parsePaginationParams(req.query);
-     const { sortBy, sortOrder } = parseSortParams(req.query);
+ export const getContactsController = async (req, res, next) => {
+   const { page, perPage } = parsePaginationParams(req.query);
+   const { sortBy, sortOrder } = parseSortParams(req.query);
+   const filter = parseFilterParams(req.query);
 
-    const contacts = await getAllContacts({
-      page,
-      perPage,
-      sortBy,
-      sortOrder,
-    });
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-};
+   const contacts = await getAllContacts({
+     page,
+     perPage,
+     sortBy,
+     sortOrder,
+     filter,
+   });
+   
+    if (contacts.data.length === 0) {
+      next(createHttpError(404, 'Contacts not found'));
+      return;
+    }
+
+   res.status(200).json({
+     status: 200,
+     message: 'Successfully found contacts!',
+     data: contacts,
+   });
+ };
+ 
 
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
