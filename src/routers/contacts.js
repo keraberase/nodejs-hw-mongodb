8b7ab10aea5398d57425f5f5c6fd842743054213
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-25 12:52:52 +0100

    add pagination to GET-all-contacts

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,6 +4,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { updateContactSchema, createContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
+
 import {
   getContactsController,
   getContactByIdController,
@@ -13,8 +14,9 @@ import {
 } from '../controllers/contacts.js';
 const router = Router();
 
+router.use('/contacts/:contactId', isValidId);
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', validateBody(updateContactSchema),ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
