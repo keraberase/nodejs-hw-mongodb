e3b5cc8c205d80a01fe08de082ca76e1b866c7fc
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-25 17:48:58 +0100

    update params of sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,8 +3,8 @@ import { ContactsCollection } from '../db/models/contact.js';
  import { SORT_ORDER } from '../constants/index.js';
 
 export const getAllContacts = async ({
-  page,
-  perPage,
+  page = 1,
+  perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
 }) => {
@@ -21,7 +21,7 @@ export const getAllContacts = async ({
     .limit(limit)
     .sort({ [sortBy]: sortOrder })
     .exec();
-    
+
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
   return {
