a2958b74b85ea013713199df1ae6ef4154e90e85
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-29 16:03:19 +0100

    add authenticate.js to middlewares

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { updateContactSchema, createContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 
 import {
@@ -14,6 +15,7 @@ import {
 } from '../controllers/contacts.js';
 const router = Router();
 
+router.use(authenticate);
 router.use('/:contactId', isValidId);
 router.get('/', ctrlWrapper (getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
