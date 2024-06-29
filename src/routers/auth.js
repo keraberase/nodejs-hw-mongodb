fc994334702636c237510a1ce20432b2d13a3eb7
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-29 15:14:25 +0100

    add refreshSession and Post-refresh-router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
-import { loginUserController, registerUserController } from '../controllers/auth.js';
+import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
@@ -12,4 +12,7 @@ router.post(
   ctrlWrapper(registerUserController),
 );
 router.post('/login', validateBody(loginUserSchema),ctrlWrapper(loginUserController));
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+router.post('/logout', ctrlWrapper(logoutUserController));
+
 export default router;
